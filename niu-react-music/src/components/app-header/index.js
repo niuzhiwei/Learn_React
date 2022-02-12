828b97beaf741f52e5a874e79fa4a8f6efc14d43
niuzhiwei <xujin02@qq.com>
2022-02-12 21:32:03 +0800

    header done

--- niu-react-music/src/components/app-header/index.js
+++ niu-react-music/src/components/app-header/index.js
@@ -1,14 +1,26 @@
 import React, { memo } from "react";
 import { NavLink } from "react-router-dom";
+import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 import { HeaderWrapper, HeaderLeft, HeaderRight } from "./style";
 import { headerLinks } from "@/common/local-data";
 
 const HYAppHeader = memo(() => {
     const showSelectItem = (item, index) => {
         if (index < 3) {
-            return <NavLink to={item.link}>{item.title}</NavLink>;
+            return (
+                <NavLink exact to={item.link}>
+                    {item.title}
+                    <i className="sprite_01 icon"></i>
+                </NavLink>
+            );
         } else {
-            return <a href={item.link}>{item.title}</a>;
+            return (
+                <a href={item.link}>
+                    {item.title}
+                    <i className="sprite_01 icon"></i>
+                </a>
+            );
         }
     };
 
@@ -20,17 +32,22 @@ const HYAppHeader = memo(() => {
                     <div className="select-list">
                         {headerLinks.map((item, index) => {
                             return (
-                                <div key={item.title}>
+                                <div className="select-item" key={item.title}>
                                     {showSelectItem(item, index)}
                                 </div>
                             );
                         })}
                     </div>
                 </HeaderLeft>
-                <HeaderRight></HeaderRight>
-                <NavLink to="/">发现音乐</NavLink>
-                <NavLink to="/mine">我的音乐</NavLink>
-                <NavLink to="/friend">我的朋友</NavLink>
+                <HeaderRight>
+                    <Input
+                        className="search"
+                        placeholder="音乐视频电台用户"
+                        prefix={<SearchOutlined />}
+                    ></Input>
+                    <div className="center">创作者中心</div>
+                    <div className="center">登录</div>
+                </HeaderRight>
             </div>
             <div className="divider"></div>
         </HeaderWrapper>
